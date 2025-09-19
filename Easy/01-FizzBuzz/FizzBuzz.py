4663acaa955a31649553322f49829cf4f2c28aeb
yaaseenk <yaaseenkhan02@gmail.com>
2025-09-19 10:06:49 -0400

    started the for loop and now it will itterate through n range and append Fizz if i /3 has a reaminder of 0

--- Easy/01-FizzBuzz/FizzBuzz.py
+++ Easy/01-FizzBuzz/FizzBuzz.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        results = []
+        for i in range(1, n + 1):
+            if i % 3 == 0:
+                results.append("Fizz")
+            else: results.append(i)
         
+        print(results)
+
+    fizzBuzz(10, 30)
