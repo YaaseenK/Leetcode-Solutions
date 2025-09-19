ba1a5f010d6723f3fb5fd2b46c9684527fc7a2fa
yaaseenk <yaaseenkhan02@gmail.com>
2025-09-19 09:28:22 -0400

    finished the problem

--- Easy/01-FizzBuzz/FizzBuzz.js
+++ Easy/01-FizzBuzz/FizzBuzz.js
@@ -3,6 +3,20 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-
-    
+    let result = []
+    for (let i = 1; i <= n; i ++){
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push('FizzBuzz');
+        } else if (i % 3 === 0){
+            result.push('Fizz')
+        } else if (i % 5 === 0){
+            result.push('Buzz')
+        }
+        else {
+            result.push(i)
+        }
+    }
+    console.log(result) ;
 }
+
+fizzBuzz(30)
