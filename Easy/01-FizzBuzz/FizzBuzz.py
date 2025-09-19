db4493d12e43cd915c889820728d618088d3224a
yaaseenk <yaaseenkhan02@gmail.com>
2025-09-19 11:04:55 -0400

    added comments to the code

--- Easy/01-FizzBuzz/FizzBuzz.py
+++ Easy/01-FizzBuzz/FizzBuzz.py
@@ -4,15 +4,19 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        # made a results array
         results = []
+        # created a for loop with range class (start at 1, end at n + 1 because it finishs before the end number)
         for i in range(1, n + 1):
+            # checks to see if i is divisible by 3 or 5 and if true then append "FizzBuzz"
             if i % 3 == 0 and i % 5 == 0:
                 results.append("FizzBuzz")
+                # checks to see if i is divisable by 3
             elif i % 3 == 0:
                 results.append("Fizz")
+            # checks to see if i is divisable by 5
             elif i % 5 == 0:
                 results.append("Buzz")
-
             else: results.append(str(i))
         
         return results
