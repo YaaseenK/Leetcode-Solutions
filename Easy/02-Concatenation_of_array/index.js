3d5037d8a43b62b362785774f60e79633884c48d
yaaseenk <yaaseenkhan02@gmail.com>
2025-09-20 15:07:06 -0400

    completed the problem

--- Easy/02-Concatenation_of_array/index.js
+++ Easy/02-Concatenation_of_array/index.js
@@ -7,8 +7,16 @@ var getConcatenation = function(nums) {
     let n = nums.length
     // using the Array constructor we can pre-allocate (2*n) space to a new array
     let ans = new Array(2*n)
-
-    
+    // loop through the original nums array
+        for(let i = 0; i < n; i++){
+            // fill the ans array with the original array 
+            ans[i] = nums[i];
+            // populate the second half of the array with same element
+            // n + i shifts the index into the second half of the array. 
+            // example: if n=3; i=0 -> ans[3] = num[i]
+            ans[i] = num[i]
+        }
+        console.log(ans)
     }
 
 nums1 = [1,2,3,4]
