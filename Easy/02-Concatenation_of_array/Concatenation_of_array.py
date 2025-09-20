cec72f6be40dd5c458fe6384cb4a0df4c0d81945
yaaseenk <yaaseenkhan02@gmail.com>
2025-09-20 15:47:01 -0400

    created a python file with the boiler plate code for concatenation of arrays problem

---
+++ Easy/02-Concatenation_of_array/Concatenation_of_array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+
+s = Solution()
+num = [1,2,4,5,6]
+print(s.getConcatenation(num))
