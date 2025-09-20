c1ddc0c7cde4af9da3015a69a422b14fde405e19
yaaseenk <yaaseenkhan02@gmail.com>
2025-09-20 15:03:23 -0400

    created n = nums.length and created a new array with t2*n space

--- Easy/02-Concatenation_of_array/index.js
+++ Easy/02-Concatenation_of_array/index.js
@@ -3,7 +3,12 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
+    // we will get thee length of nums
+    let n = nums.length
+    // using the Array constructor we can pre-allocate (2*n) space to a new array
+    let ans = new Array(2*n)
 
+    
     }
 
 nums1 = [1,2,3,4]
