751049041d193e9fe34e954a6dd6a2baea67d466
yaaseenk <yaaseenkhan02@gmail.com>
2025-09-20 16:52:11 -0400

    added a import and completed the code but there is an error with the output where the array is not writing over the place holders past the first input

--- Easy/02-Concatenation_of_array/main.py
+++ Easy/02-Concatenation_of_array/main.py
@@ -1,15 +1,20 @@
+from typing import List
 class Solution(object):
-    def getConcatenation(self, nums):
+    def getConcatenation(self, nums: List[int]) -> List[int]:
         """
         :type nums: List[int]
         :rtype: List[int]
         """
         # find the length of nums 
         n = len(nums)
-        return n
-        
+        # create a list double the orginal with placeholders
+        ans = [0] * (2 * n)
+        for i in range(n):
+            ans[i] = nums[i]
+            ans[i+n] = nums[i]
+            return ans
 
 
 s = Solution()
-num = [1,2,4,5,6]
-print(s.getConcatenation(num))
+a = [1,2,3]
+print(s.getConcatenation(a))
