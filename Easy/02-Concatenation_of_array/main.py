7022753f2fbfe109ea3a8cb47e896756b045bbfb
yaaseenk <yaaseenkhan02@gmail.com>
2025-09-21 09:25:38 -0400

    added comments to the python code

--- Easy/02-Concatenation_of_array/main.py
+++ Easy/02-Concatenation_of_array/main.py
@@ -9,8 +9,13 @@ class Solution(object):
         n = len(nums)
         # create a list double the orginal with placeholders
         ans = [0] * (2 * n)
+        # loop the original list
         for i in range(n):
+            # fills the frist part of the array with the items from the first array
             ans[i] = nums[i]
+            # Populate the second half of the array with the same element
+            # n + i shift the index into the second half of the array
+            # example: if n = 3; i = 0; ans[3] = num[1]
             ans[i+n] = nums[i]
         return ans
 
