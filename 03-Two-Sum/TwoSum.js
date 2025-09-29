9139d554fc3ce27eca669a9e3c9c43eab3278c1c
Yaaseen <yaaseenkhan02@gmail.com>
2025-09-29 03:18:31 -0400

    added comments

--- 03-Two-Sum/TwoSum.js
+++ 03-Two-Sum/TwoSum.js
@@ -4,17 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // Creating a map
     const map = new Map()
-    const map = new Map()
+    // n is the length of the arry ( delete later to free up memory)
     let n = nums.length
+    //  t is the target value (del later for memeory)
     let t = target
     
+    // loop through the length of the array
     for (let i = 0; i < n; i++){
-        map.set(nums[i], i)
+        map.set(nums[i], i)  // set the keys and values to our map
     }
-
+    // for loop to itterate through the length of the array
     for (let i = 0; i < n; i++ ){
-        const complement = t - nums[i]  
+        const complement = t - nums[i]  // *x + y = t WRITTEN as x = t - y* 
         // console.log('i right now:'  + i )
         console.log('complement: ' + map.get(complement))
         if(map.has(complement) && map.get(complement) !== i){
