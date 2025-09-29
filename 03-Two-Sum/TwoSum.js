b42feb38dc2290c8d6747bd8bd5df55bb7739f22
Yaaseen <yaaseenkhan02@gmail.com>
2025-09-29 02:56:47 -0400

    working out the algorithm for two sum

--- 03-Two-Sum/TwoSum.js
+++ 03-Two-Sum/TwoSum.js
@@ -5,15 +5,27 @@
  */
 var twoSum = function(nums, target) {
     const map = new Map()
+    const map = new Map()
     let n = nums.length
     let t = target
     
     for (let i = 0; i < n; i++){
         map.set(nums[i], i)
     }
-    console.log(map)
+
+    for (let i = 0; i < n; i++ ){
+        const complement = t - nums[i]  
+        // console.log('i right now:'  + i )
+        console.log('complement: ' + map.get(complement))
+        if(map.has(complement) && map.get(complement) !== i){
+            return console.log('completment:' + complement + ' i:' + nums[i])
+        }
+        
+    }
+
+
 } 
 
-let aray = [3, 5, 6, 7, 8, 9, -4]
+let aray = [5, 6, 8, 9, -4, 7 , 3]
 let target = 10
 twoSum(aray, target)
