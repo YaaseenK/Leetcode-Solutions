7d29d8526538503f7563cb095b50671e613a1715
yaaseenk <yaaseenkhan02@gmail.com>
2025-10-01 23:29:08 -0400

    took the period out of the file name

---
+++ Easy/01-FizzBuzz/FizzBuzz_2.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    // Create an array to return 
+    // let ans = [];
+    // for (let i = 1; i <= n; i++ ){
+    //     let s = ''
+    //     if (i % 3 === 0) s += 'Fizz';
+    //     if (i % 5 === 0) s += 'Buzz';
+    //     ans[i - 1] = s || i.toString();
+    // }
+    // console.log(ans)
+
+    let res = [];
+    for (let i = 1; i <= 0; i++){
+        console.log(i)
+    }
+
+}
+
+fizzBuzz(30)
