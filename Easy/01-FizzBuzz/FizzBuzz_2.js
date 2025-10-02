740aab67408d5d2cd849b88742958ec59a90aa6c
yaaseenk <yaaseenkhan02@gmail.com>
2025-10-01 23:35:06 -0400

    redid the fizzbuzz code for learnability

--- Easy/01-FizzBuzz/FizzBuzz_2.js
+++ Easy/01-FizzBuzz/FizzBuzz_2.js
@@ -10,14 +10,18 @@ var fizzBuzz = function(n) {
     //     if (i % 3 === 0) s += 'Fizz';
     //     if (i % 5 === 0) s += 'Buzz';
     //     ans[i - 1] = s || i.toString();
-    // }
+    // }        
     // console.log(ans)
+    // let res = [];
+    // for (let i = 1; i <= n; i++){
+    //     s = '';
+    //     if (i % 3 === 0) s += "Fizz"
+    //     if (i % 5 === 0) s += "Buzz"
+    //     res[i - 1] = s || i.toString()
+    // }
+    // console.log(res)
 
     let res = [];
-    for (let i = 1; i <= 0; i++){
-        console.log(i)
-    }
-
 }
 
 fizzBuzz(30)
