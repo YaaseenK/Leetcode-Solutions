b10d2417538f0f9a06a46b399dd07c947ea687cd
yaaseenk <yaaseenkhan02@gmail.com>
2025-10-01 23:11:41 -0400

    redoing the fizz buzz problem with better space complexity

---
+++ Easy/01-FizzBuzz/Index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    // Create an array to return 
+    ans = [];
+    for (let i = 0; i <= n.length; i++ ){
+        console.log(i)
+    }
+    console.log(result) ;
+}
+
+fizzBuzz(30)
