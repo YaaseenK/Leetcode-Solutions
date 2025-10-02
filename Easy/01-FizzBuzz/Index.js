8e407c48dcb0890bb1e3cfda22da50fec432b447
yaaseenk <yaaseenkhan02@gmail.com>
2025-10-01 23:15:16 -0400

    added a for loop which does itterate through n starting at 1 and printing to n

--- Easy/01-FizzBuzz/Index.js
+++ Easy/01-FizzBuzz/Index.js
@@ -4,11 +4,11 @@
  */
 var fizzBuzz = function(n) {
     // Create an array to return 
-    ans = [];
-    for (let i = 0; i <= n.length; i++ ){
+    let ans = [];
+    for (let i = 1; i <= n; i++ ){
         console.log(i)
     }
-    console.log(result) ;
+
 }
 
 fizzBuzz(30)
