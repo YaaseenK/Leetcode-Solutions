db0abb3f37cb26cb2e073584fff168b0393b8d80
yaaseenk <yaaseenkhan02@gmail.com>
2025-10-16 11:08:37 -0400

    commented out the algo so I can redo it

--- Easy/04-FisherYates/index.js
+++ Easy/04-FisherYates/index.js
@@ -1,21 +1,22 @@
-let arr = ['a', 'b', 'c'];
+// let arr = ['a', 'b', 'c'];
 
-for (let i = 0; i < 10; i ++){
-    arr.push(i)
-}
-console.log(arr)
+// for (let i = 0; i < 10; i ++){
+//     arr.push(i)
+// }
+// console.log(arr)
 
-function shuff (arr) {
-    for (i = arr.length - 1; i >= 0; i--){
-        // console.log(arr[i])
-        const j = randInt(i + 1);
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
-}
+// function shuff (arr) {
+//     for (i = arr.length - 1; i >= 0; i--){
+//         // console.log(arr[i])
+//         const j = randInt(i + 1);
+//         [arr[i], arr[j]] = [arr[j], arr[i]];
+//     }
+//     return arr;
+// }
 
-function randInt (maxExclusive){
-    return Math.floor(Math.random() * maxExclusive)
-}
+// function randInt (maxExclusive){
+//     return Math.floor(Math.random() * maxExclusive)
+// }
 
-console.log(shuff(arr))
+// console.log(shuff(arr));
+
