1385cbc82203b37921c2f87db3d46c7411d502c4
yaaseenk <yaaseenkhan02@gmail.com>
2025-11-16 06:15:11 -0500

    doing two sum in python

---
+++ Easy/01-FizzBuzz/fizz_buzz.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let res =  [];
+    for (let i = 1; i <= n; i++){
+        let s = '';
+        if (i % 3 === 0) s += 'Fizz';
+        if (i % 5 === 0) s += 'Buzz';
+        res[i-1] = s || i.toString();
+    }
+    return res;
+}
+
+console.log(fizzBuzz(30));
