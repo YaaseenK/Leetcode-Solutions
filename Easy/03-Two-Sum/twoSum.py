e89b5e7a81d611bbe4ca1f30405701fd4a2628e7
yaaseenk <yaaseenkhan02@gmail.com>
2025-11-16 06:55:40 -0500

    two sums in O(1) time complexity

--- Easy/03-Two-Sum/twoSum.py
+++ Easy/03-Two-Sum/twoSum.py
@@ -1,9 +1,23 @@
+from typing import List
+
+nums = [2, 7, 11, 15]
+target = 9
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # for i in range (len(nums)):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[j] == target - nums[i]:
-        #             return[i,j]
-        #             return []
         hashmap = {}
-        
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+#         # If no valid pair is found, return an empty list
+#         return []
+        return hashmap
+
+# # Create an instance of the Solution class
+solution = Solution()
+
+# Call the twoSum method using the solution instance
+print(solution.twoSum(nums, target))
